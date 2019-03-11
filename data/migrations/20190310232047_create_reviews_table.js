dd05fb584eb45911376852f4f3285556eb5c6d8b
Nate Boyette <nlboyette@gmail.com>
2019-03-10 23:24:16 -0700

    Create reviews table with columns for text content and a number rating

---
+++ data/migrations/20190310232047_create_reviews_table.js
@@ -0,0 +1,26 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("reviews", tbl => {
+    tbl.increments("review_id");
+
+    tbl.string("review_content", 255).notNullable();
+    tbl.integer("rating").unsigned();
+
+    tbl.integer("user_id").unsigned();
+    tbl
+      .foreign("user_id")
+      .references("user_id")
+      .on("users");
+
+    tbl.integer("driver_id").unsigned();
+    tbl
+      .foreign("driver_id")
+      .references("drivers")
+      .on("drivers");
+
+    tbl.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("drivers");
+};

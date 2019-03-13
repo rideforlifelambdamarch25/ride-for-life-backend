c70bafd2ac93fbe8ebc85baf48f0c0898e51a78e
Nate Boyette <nateboyette@icloud.com>
2019-03-12 23:24:56 -0700

    Refactored the user table.

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -10,63 +10,35 @@ exports.seed = function(knex, Promise) {
       return knex("users").insert([
         {
           firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
           phone: fake.phone.phoneNumber(),
-          user_type: "mother",
-          location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
+          location: `${fake.address.latitude()}, ${fake.address.longitude()}`
         },
         {
           firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
           phone: fake.phone.phoneNumber(),
-          user_type: "mother",
-          location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
+          location: `${fake.address.latitude()}, ${fake.address.longitude()}`
         },
         {
           firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
           phone: fake.phone.phoneNumber(),
-          user_type: "mother",
           location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
         },
         {
           firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
           phone: fake.phone.phoneNumber(),
-          user_type: "caretaker",
-          location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
+          location: `${fake.address.latitude()}, ${fake.address.longitude()}`
         },
         {
-          firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
-          phone: fake.phone.phoneNumber(),
-          user_type: "mother",
+          firstname: fake.name.firstName(),   
+          phone: fake.phone.phoneNumber(),          
           location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
+          
         },
         {
-          firstname: fake.name.firstName(),
-          lastname: fake.name.lastName(),
+          firstname: fake.name.firstName(),          
           phone: fake.phone.phoneNumber(),
-          user_type: "caretaker",
           location: `${fake.address.latitude()}, ${fake.address.longitude()}`,
-          username: fake.internet.userName(),
-          password: bcrypt.hashSync("password", 12),
-          email: fake.internet.email()
+          
         }
       ]);
     });
